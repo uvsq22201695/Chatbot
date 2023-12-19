2a80254ad4775731b121e312be1e685ae46e0107
Alexandre François <alexandre.francois@ens.uvsq.fr>
2023-12-19 02:54:28 +0100

    (main.py) Major changes: retry bouton, undo bouton, submit bouton and interface improve

--- ia.py
+++ ia.py
@@ -5,14 +5,14 @@ with open('data.json', 'r', encoding="utf-8") as json_file:
     data = json.load(json_file)
 
 
-def get_response(userInput):
+def get_response(userInput: str) -> str:
     """
     Get the best response from the IA
     :param userInput: String to be analyzed
     :return best_match: Best response from the IA
     """
 
-    user_tokens = tokenize(userInput)
+    user_tokens = tokenize(userInput.lower())
 
     best_match = None
     best_similarity = 0.0
--- main.py
+++ main.py
@@ -3,28 +3,58 @@ from ia import get_response
 
 PITCH = "Hello, i'm a chatbot."  # TODO: Default message of the chatbot
 
-with gr.Blocks(css="index.css", title="IA Chatbot", theme="soft") as interface:  # TODO: Change the website title
+with gr.Blocks(css="index.css", title="IA Chatbot", theme="monochrome") as interface:  # TODO: Change the website title
 
     gr.Markdown(value="Chatbot", elem_classes="chatbot_header")  # TODO: You can change the title of the chatbot
 
-    chatbot = gr.Chatbot(value=[[None, PITCH]], show_share_button=True, show_copy_button=True,
-                         avatar_images=("icons/human.png", "icons/robot.png"))
-    msg = gr.Textbox(label="Prompt", autofocus=True, show_copy_button=True)
-    clear = gr.Button(value="Clear chat history")  # TODO: You can change the text of the button
+    with gr.Group():
+        chatbot = gr.Chatbot(value=[[None, PITCH]], show_share_button=True, show_copy_button=True,
+                             avatar_images=("icons/human.png", "icons/robot.png"))
+
+        with gr.Row():
+            msg = gr.Textbox(show_label=False, placeholder="Type a message...", autofocus=True,
+                             scale=7)
+            submit_btn = gr.Button(value="Submit", scale=1)  # TODO: You can change the text of the button
+
+    with gr.Row():
+        retry_btn = gr.Button(value="Retry")  # TODO: You can change the text of the button
+        undo_btn = gr.Button(value="Undo")  # TODO: You can change the text of the button
+        clear_btn = gr.Button(value="Clear")  # TODO: You can change the text of the button
 
 
-    def respond(message, chat_history):
-        chat_history.append((message, get_response(message.lower())))
-        return "", chat_history
+        def respond(message, chat_history):
+            if message == "":
+                return "", chat_history
+
+            chat_history.append((message, get_response(message)))
+            return "", chat_history
+
+        def retry(chat_history):
+            if len(chat_history) == 1:
+                return [[None, PITCH]]
+
+            message = chat_history[-1][0]
+            chat_history.pop()
+
+            chat_history.append((message, get_response(message)))
+            return chat_history
+
+        def undo(chat_history):
+            if len(chat_history) == 1:
+                return "", chat_history
+
+            return chat_history[-1][0], chat_history[:-1]
+
+        def clear():
+            return [[None, PITCH]]
 
 
-    def clear_chat():
-        return [[None, PITCH]]
+        msg.submit(fn=respond, inputs=[msg, chatbot], outputs=[msg, chatbot], show_progress="minimal")
+        submit_btn.click(fn=respond, inputs=[msg, chatbot], outputs=[msg, chatbot], show_progress="minimal")
 
-
-    msg.submit(fn=respond, inputs=[msg, chatbot], outputs=[msg, chatbot], show_progress="minimal")
-    clear.click(fn=clear_chat, outputs=chatbot, show_progress="minimal")
-
+        retry_btn.click(fn=retry, inputs=chatbot, outputs=chatbot, show_progress="minimal")
+        undo_btn.click(fn=undo, inputs=chatbot, outputs=[msg, chatbot], show_progress="minimal")
+        clear_btn.click(fn=clear, outputs=chatbot, show_progress="minimal")
 
 if __name__ == "__main__":
     interface.launch()
